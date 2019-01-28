7909884bb81d7bc56fa92d8156c5cca78240b909
Michael Wolfe <michael.wolfe101@gmail.com>
2019-01-28 00:20:41 -0500

    set up post route

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-import express from 'express';
-import { connect } from 'mongoose';
-import bodyParser from 'body-parser';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-
+const db = require('./models');
 const app = express();
-connect('mongodb://localhost:27017/blog');
 
-app.engine('html', require(ejs).rednerFile);
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+mongoose.connect('mongodb://localhost:27017/blog');
+
+// Routes
 app.get('/', (req, res) => {
-  res.render('index');
+  db.Post.find({}, (err, posts) => {
+    res.render('index', {
+      posts: posts
+    })
+  });
+});
+
+app.post('/addpost', (req, res) => {
+  let postData = new db.Post(req.body);
+  postData
+    .save()
+    .then(result => {
+      res.redirect('/');
+    }).catch(err => {
+      res.status(400).send("Unable to sava data.");
+    });
 });
 
 app.listen(3000, () => {
